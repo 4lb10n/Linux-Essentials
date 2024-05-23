a04c960ee776cbba92916c8db546c75b54601d83
KaiserAlbion <sektelan@gmail.com>
2024-05-23 13:55:26 +0200

    Create heartbeat_fun.sh

---
+++ Scripte/heartbeat_fun.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+handleHup()
+
+{
+        echo "HJUP"
+        exit 0
+}
+handleExit()
+
+{
+        echo "EXIT"
+}
+
+trap handleHup 1
+trap handleExit 0
+
+while true
+do
+    echo Beep
+    sleep 5
+done
+
+# Jetzt kommt wichtgier code, der nicht unterbrochen werden darf.
+trap "" 2 3
+
+# Kdo 1
+# Kdo 2
+# Kdo 3
+# Reset the trapping
+trap - 2 3
