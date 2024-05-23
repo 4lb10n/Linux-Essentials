fd781d22766034a79c01bec39ca6cef892ad0cfa
KaiserAlbion <sektelan@gmail.com>
2024-05-23 13:25:36 +0200

    Update heartbeat.sh

--- Scripte/heartbeat.sh
+++ Scripte/heartbeat.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-trap "echo Nope >&2" 2
+#trap "echo Nope >&2" 1 2 3 15 
 
 while true
 do
     echo Beep
     sleep 5
 done
+
+# Jetzt kommt wichtgier code, der nicht unterbrochen werden darf.
+trap "" 2 3
+
+# Kdo 1
+# Kdo 2
+# Kdo 3
+# Reset the trapping
+trap - 2 3
