80844041e429218c774accc2629a26e5f6a8feae
4lb10n <sektelan@gmail.com>
2024-06-03 14:47:34 +0200

    Update heartbeat.sh

--- Scripte/heartbeat.sh
+++ Scripte/heartbeat.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-#trap "echo Nope >&2" 1 2 3 15 
+beatCount=0
+
+handleHup()
+
+{
+    beatCount=0
+}
+handleExit()
+
+{
+    echo "EXIT" >&2
+}
+
+trap handleHup 1
+trap handleExit 0
 
 while true
 do
-    echo Beep
+    let beatCount++
+    echo Beep $beatCount
     sleep 5
 done
-
-# Jetzt kommt wichtgier code, der nicht unterbrochen werden darf.
-trap "" 2 3
-
-# Kdo 1
-# Kdo 2
-# Kdo 3
-# Reset the trapping
-trap - 2 3
